b77bffc3d6d4f77f6265107983cf890ddfa6aa72
Jesper Vehn Munk <jemun@mobilelife.dk>
2018-06-22 16:37:25 +0200

    Changed event allocation.

--- index.js
+++ index.js
@@ -24,17 +24,17 @@ app.get('/api/passwords', (req, res) => {
 
 app.get('/api/events', (req, res) => {
   res.json([      {Id:1, Name:'Mål!!!', RefereeName:'Spiller bolden', Type:'Other3'},
-                  {Id:2, Name:'Assist', RefereeName:'Adskiller spillere', Type:'Other2'},
-                  {Id:3, Name:'Skud på mål', RefereeName:'1H 3+ min', Type:'Other1'},
+                  {Id:2, Name:'Assist', RefereeName:'Sætter sig igennem', Type:'Other2'},
+                  {Id:3, Name:'Skud på mål', RefereeName:'Spray (1/situation)', Type:'Other1'},
                   {Id:4, Name:'Tager hjørne', RefereeName:'Publikum spotter kamera', Type:'Other1'},
                   {Id:5, Name:'Medic!', RefereeName:'1H 0-2 min', Type:'Own1'},
-                  {Id:6, Name:'Offside', RefereeName:'2H 4+ min', Type:'Own1'},
+                  {Id:6, Name:'Offside', RefereeName:'VAR/GLT', Type:'Own1'},
                   {Id:7, Name:'Frispark begået', RefereeName:'Dommerkast', Type:'Own1'},
                   {Id:8, Name:'Skud udenfor mål', RefereeName:'2H 0-3 min', Type:'Own1'},
                   {Id:9, Name:'Straffe begået', RefereeName:'Forholder sig til tiden', Type:'Own2'},
-                  {Id:10, Name:'Gult kort', RefereeName:'Spray (1/situation)', Type:'Own2'},
-                  {Id:11, Name:'Udskiftet', RefereeName:'Ramt af bold/spiller', Type:'Own2'},
-                  {Id:12, Name:'Selvmål', RefereeName:'VAR/GLT', Type:'Own3'},
+                  {Id:10, Name:'Gult kort', RefereeName:'1H 3+ min', Type:'Own2'},
+                  {Id:11, Name:'Udskiftet', RefereeName:'2H 4+ min', Type:'Own2'},
+                  {Id:12, Name:'Selvmål', RefereeName:'Ramt af bold/spiller', Type:'Own3'},
                   {Id:13, Name:'Brændt straffe', RefereeName:'Falder', Type:'Own3'},
                   {Id:14, Name:'Rødt kort', RefereeName:'Udskiftes', Type:'Own3'}]);
 });
