23f6cadb9b95cec5d9e622add80c15897a31f14c
Jesper Vehn Munk <pilotlarsen@gmail.com>
2022-11-15 20:13:20 +0100

    Moved GameMenu top right.

--- client/src/components/game/gameMenu.js
+++ client/src/components/game/gameMenu.js
@@ -3,11 +3,12 @@ import Wipe from './wipe';
 import Loader from './loader';
 import GraphSelector from './graphSelector';
 import SoundToggle from './soundToggle';
+import './game.css';
 
 export default class GameMenu extends React.Component {
   render() {
     return(
-      <div>
+      <div className='gameMenuDiv'>
         <Wipe wipe={this.props.wipe}/>
         <Loader gameDataLoaded={this.props.gameDataLoaded}/>
         <GraphSelector onClick={this.props.onClick} />
